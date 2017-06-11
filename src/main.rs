06a4e843cf16d25132c33f24edf91367b8f46b64
Aaron Hill <aa1ronham@gmail.com>
2017-06-11 13:30:41 -0400

    Support using tty

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 extern crate regex;
+extern crate libc;
+
+mod tty;
 
 use std::io::BufReader;
 use regex::Regex;
@@ -6,22 +9,30 @@ use std::process::{Command, Stdio};
 use std::io::BufRead;
 
 const CARGO: &'static str = "cargo";
-const CRATE_LEN: usize = 26; // 99% of crates have names <= 16 chars at time of writing
+const CRATE_LEN: usize = 26; // 99% of crates have names <= 26 chars at time of writing
 const VERSION_LEN: usize = 6; // Typical form of 'vX.Y.Z'
 
 fn main() {
     // https://www.npmjs.com/package/ansi-regex
     let regex = Regex::new("[\u{001b}\u{009b}][\\[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-PRZcf-nqry=><]").unwrap();
 
-    let args = vec!["--color=always".to_owned()];
+    //let args = vec!["--color=always".to_owned()];
+    let args = vec![];
 
-    let child = Command::new(CARGO)
-        .args(std::env::args().skip(1).chain(args.into_iter()))
-        .stderr(Stdio::piped())
-        .spawn().unwrap();
-    let reader = BufReader::with_capacity(10, child.stderr.unwrap());
-    for line in reader.lines() {
-        let line = line.unwrap();
+    let mut command = Command::new(CARGO);
+    command.args(std::env::args().skip(1).chain(args.into_iter()))
+        .stdout(Stdio::inherit());
+
+    let mut reader: BufReader<tty::Handle> = BufReader::with_capacity(10, tty::get_handle(command));
+    let newline: &[_] = &['\n', '\r'];
+    loop {
+        let mut line = String::new();
+        let line = match reader.read_line(&mut line) {
+            Ok(_) => line.trim_right_matches(newline),
+            Err(e) => {
+                if tty::handle_err(reader.get_mut(), e) { break } else { continue };
+            }
+        };
         let tmp = regex.replace_all(&line, "");
 
         let clean = tmp.trim_left();
@@ -36,3 +47,5 @@ fn main() {
         }
     }
 }
+
+
---
+++ src/tty.rs
@@ -0,0 +1,73 @@
+use std::process::{Command, Stdio, ChildStderr, Child};
+use std::ptr;
+use std::io::{Read, Error};
+use std::fs::File;
+
+#[cfg(target_os = "linux")]
+pub fn get_handle(mut command: Command) -> Handle {
+	use libc::{self, winsize, c_int, TIOCGWINSZ};
+	use std::os::unix::io::FromRawFd;
+	use std::fs::File;
+
+    let mut master: c_int = 0;
+    let mut slave: c_int = 0;
+
+    let mut win = winsize {
+        ws_row: 0,
+        ws_col: 0,
+        ws_xpixel: 0,
+        ws_ypixel: 0,
+    };
+
+    let res = unsafe {
+		libc::ioctl(0, TIOCGWINSZ, &mut win);
+        libc::openpty(&mut master, &mut slave, ptr::null_mut(), ptr::null(), &win)
+    };  
+
+    if res < 0 { 
+        panic!("Failed to open pty: {}", res);
+    }
+
+	let child = command.stderr(unsafe { Stdio::from_raw_fd(slave) } )
+		.spawn()
+		.unwrap();
+
+    unsafe {
+		Handle::Pty {child: child, fd: master as usize, file: File::from_raw_fd(master) }
+	}
+}
+
+pub enum Handle {
+	Pty {child: Child, fd: usize, file: File },
+	Stderr(Child, ChildStderr)
+}
+
+impl Read for Handle {
+	fn read(&mut self, buf: &mut [u8]) -> Result<usize, Error> {
+		match *self {
+			Handle::Pty{ref mut file, ..} => file.read(buf),
+			Handle::Stderr(.., ref mut stderr) => stderr.read(buf) 
+		}
+	}
+}
+
+
+#[cfg(target_os = "linux")]
+pub fn handle_err(handle: &mut Handle, err: Error) -> bool {
+	if let Handle::Pty {ref mut child, ..} = *handle {
+		if child.try_wait().unwrap().is_some() {
+			return true;
+		}
+	}
+	panic!("Error: {:?}", err)
+}
+
+
+#[cfg(not(target_os = "linux"))]
+pub fn get_handle(mut command: Command) -> Handle {
+	let child = command.stderr(Stdio::piped())
+		.spawn()
+		.unwrap();
+
+	Handle::Stderr(child.stderr.unwrap())
+}
